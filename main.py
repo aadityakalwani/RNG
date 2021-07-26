24b0053dc3464a314218c53ed8f89d130d9dd78d
aadityakalwani <aadityakalwani@gmail.com>
2021-07-26 12:22:02 +0100

    deleted unnecessary stuff

--- main.py
+++ main.py
@@ -27,30 +27,4 @@ def first_rng():
                 continue
 
 
-def new_rng():
-    while True:
-        x = int(input("Enter the first number: "))
-        y = int(input("Enter the second number: "))
-
-        print(f"Your random number is {random.randint(x, y)}")
-
-        repeat_or_not = input("Press q to quit, or any other key to repeat")
-
-        if repeat_or_not == "q":
-            print("Thanks for using my RNG")
-            break
-
-        elif repeat_or_not != "q":
-            same_numbers = input("Press y if you want the random number to be within the same range\n"
-                                 "Press n if you want the random number to be within a different range\n"
-                                 "Enter here:")
-
-            if same_numbers == "y":
-                print(f"Your random number is {random.randint(x, y)}")
-                continue
-
-            elif same_numbers == "n":
-                continue
-
-
 first_rng()
